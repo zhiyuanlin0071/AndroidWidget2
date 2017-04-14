8e7db1778f13281d938644d7dfe1d21f4f776210
linzhiyuan <648238210@qq.com>
2017-04-14 17:07:09 +0800

    Initial commit

---
+++ app/src/main/java/com/zhiyuan/androidwidget/ui/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zhiyuan.androidwidget.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhiyuan.androidwidget.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+
+}
