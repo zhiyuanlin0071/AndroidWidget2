b2071b870069339e8fa7d5e564278337cc31e64f
linzhiyuan <648238210@qq.com>
2017-04-20 14:30:18 +0800

    add  loadmore data

--- app/src/main/java/com/zhiyuan/androidwidget/ui/activity/RecycleViewActivity.java
+++ app/src/main/java/com/zhiyuan/androidwidget/ui/activity/RecycleViewActivity.java
@@ -1,27 +1,36 @@
 package com.zhiyuan.androidwidget.ui.activity;
 
 import android.graphics.Rect;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.zhiyuan.androidwidget.R;
-import com.zhiyuan.androidwidget.ui.adapter.MyAdapter;
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
+import com.zhy.adapter.recyclerview.wrapper.EmptyWrapper;
+import com.zhy.adapter.recyclerview.wrapper.HeaderAndFooterWrapper;
+import com.zhy.adapter.recyclerview.wrapper.LoadMoreWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by stefan on 2017/4/14.
@@ -38,10 +47,14 @@ public class RecycleViewActivity extends BaseActivity {
 	@BindView(R.id.remove)
 	Button							mRemove;
 	private LinearLayoutManager		mLinearLayout;
-	private String[]				mCategory	= new String[]{"LinnerLayout vertical","LinnerLayout horizontal", "GridLayout", "WaterfallLayout"};
+	private String[]				mCategory	= new String[]{"LinnerLayout vertical", "LinnerLayout horizontal", "GridLayout", "WaterfallLayout"};
 	private ArrayAdapter<String>	mArrayAdapter;
 	private List<String>			mDatas		= new ArrayList<>();
-	private MyAdapter				mMyAdapter;
+	// private MyAdapter mMyAdapter;
+	private CommonAdapter<String>	mCommonAdapter;
+	private HeaderAndFooterWrapper	mHeaderAndFooterWrapper;
+	private EmptyWrapper			mEmptyWrapper;
+	private LoadMoreWrapper			mLoadMoreWrapper;
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -60,10 +73,11 @@ public class RecycleViewActivity extends BaseActivity {
 	}
 	
 	private void initRecycleView() {
-//		mLinearLayout = new LinearLayoutManager(this);
-//		mContent.setLayoutManager(mLinearLayout);
-        StaggeredGridLayoutManager layoutManager=new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL);
-        mContent.setLayoutManager(layoutManager);
+		mLinearLayout = new LinearLayoutManager(this);
+		mContent.setLayoutManager(mLinearLayout);
+		// StaggeredGridLayoutManager layoutManager = new
+		// StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL);
+		mContent.setLayoutManager(mLinearLayout);
 		mContent.setHasFixedSize(true);
 		mContent.setItemAnimator(new DefaultItemAnimator());
 		mContent.addItemDecoration(new SpaceItem(20));
@@ -75,8 +89,46 @@ public class RecycleViewActivity extends BaseActivity {
 		for (int i = 0; i < 20; i++) {
 			mDatas.add("text" + i);
 		}
-		mMyAdapter = new MyAdapter(mDatas);
-		mContent.setAdapter(mMyAdapter);
+		mCommonAdapter = new CommonAdapter<String>(this, R.layout.item_text, mDatas) {
+			@Override
+			protected void convert(ViewHolder holder, String s, int position) {
+				holder.setText(R.id.list_item, s);
+			}
+		};
+		initHead();
+		initLoadMore();
+		// mMyAdapter = new MyAdapter(mDatas);
+		mContent.setAdapter(mLoadMoreWrapper);
+		
+	}
+	
+	private void initLoadMore() {
+		mLoadMoreWrapper = new LoadMoreWrapper(mHeaderAndFooterWrapper);
+		mLoadMoreWrapper.setLoadMoreView(R.layout.default_loading);
+		mLoadMoreWrapper.setOnLoadMoreListener(new LoadMoreWrapper.OnLoadMoreListener() {
+			@Override
+			public void onLoadMoreRequested() {
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						mDatas.add("add");
+						mLoadMoreWrapper.notifyDataSetChanged();
+					}
+				}, 2000);
+			}
+		});
+	}
+	
+	private void initHead() {
+		mHeaderAndFooterWrapper = new HeaderAndFooterWrapper(mCommonAdapter);
+		TextView textView = new TextView(this);
+		textView.setText("head");
+		textView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+		textView.setGravity(Gravity.CENTER);
+		TextView textView1 = new TextView(this);
+		textView1.setText("foot");
+		mHeaderAndFooterWrapper.addHeaderView(textView);
+		mHeaderAndFooterWrapper.addFootView(textView1);
 	}
 	
 	@Override
@@ -89,16 +141,14 @@ public class RecycleViewActivity extends BaseActivity {
 						mContent.setLayoutManager(new LinearLayoutManager(RecycleViewActivity.this));
 						break;
 					case 1 :
-                        mContent.setLayoutManager(new LinearLayoutManager(RecycleViewActivity.this,LinearLayoutManager.HORIZONTAL,false));
+						mContent.setLayoutManager(new LinearLayoutManager(RecycleViewActivity.this, LinearLayoutManager.HORIZONTAL, false));
 						break;
 					case 2 :
-                        mContent.setLayoutManager(new GridLayoutManager(RecycleViewActivity.this, 3));
+						mContent.setLayoutManager(new GridLayoutManager(RecycleViewActivity.this, 3));
 						break;
-                    case 3:
-                        mContent.setLayoutManager(new StaggeredGridLayoutManager(4
-                                , StaggeredGridLayoutManager.VERTICAL
-                        ));
-                        break;
+					case 3 :
+						mContent.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL));
+						break;
 				}
 			}
 			
@@ -107,6 +157,7 @@ public class RecycleViewActivity extends BaseActivity {
 				
 			}
 		});
+		
 	}
 	
 	@Override
@@ -126,8 +177,20 @@ public class RecycleViewActivity extends BaseActivity {
 			outRect.right = space;
 			outRect.bottom = space;
 			outRect.top = space;
-//			 if (parent.getChildPosition(view) == 0)
-//			 outRect.top = space;
+			// if (parent.getChildPosition(view) == 0)
+			// outRect.top = space;
+		}
+	}
+	
+	@OnClick({R.id.add, R.id.remove})
+	public void onclick(View view) {
+		switch (view.getId()) {
+			case R.id.add :
+				// mMyAdapter.add(1);
+				break;
+			case R.id.remove :
+				// mMyAdapter.remove(1);
+				break;
 		}
 	}
 }
--- app/src/main/java/com/zhiyuan/androidwidget/ui/adapter/MyAdapter.java
+++ app/src/main/java/com/zhiyuan/androidwidget/ui/adapter/MyAdapter.java
@@ -18,36 +18,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static com.zhiyuan.androidwidget.R.array.view;
 
 /**
  * Created by stefan on 2017/4/14.
  */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-	private List<String> data = null;
-	List<Integer> mheight;
+	private List<String>	data	= null;
+	List<Integer>			mheight;
+	public interface OnitemClickListener{
+//		void onItemLongClick(View view,int posiotion);
+		void onItemClick(View view,int posiotion);
+	}
+	private View.OnClickListener mOnClickListener;
+	private OnitemClickListener mOnitemClickListener;
 
+	public void setOnitemClickListener(OnitemClickListener onitemClickListener) {
+		mOnitemClickListener = onitemClickListener;
+	}
 	public MyAdapter(List<String> data) {
 		this.data = data;
-//		mheight=new ArrayList<Integer>();//瀑布式
-//		for(int i=0;i<data.size();i++){
-//			mheight.add((int)(100+Math.random()*300));
+//		mheight = new ArrayList<Integer>();// 瀑布式
+//		for (int i = 0; i < data.size(); i++) {
+//			mheight.add((int) (100 + Math.random() * 300));
 //		}
 	}
-
+	
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_text, null);
+		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_text, parent, false);
 		ViewHolder viewHolder = new ViewHolder(view);
 		return viewHolder;
 	}
 	
 	@Override
-	public void onBindViewHolder(ViewHolder holder, int position) {
-//		ViewGroup.LayoutParams lp=holder.mTextView.getLayoutParams();
-//		lp.height=mheight.get(position);
-//		holder.mTextView.setLayoutParams(lp);
+	public void onBindViewHolder(final ViewHolder holder, int position) {
+//		ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+//		lp.height = mheight.get(position);
+//		holder.itemView.setLayoutParams(lp);
 		holder.mTextView.setText(data.get(position));
+//		holder.itemView.setOnClickListener(mOnClickListener);
+//		if (mOnitemClickListener!=null)
+//		{
+//			holder.itemView.setOnClickListener(new View.OnClickListener() {
+//				@Override
+//				public void onClick(View v) {
+//					int pos=holder.getLayoutPosition();
+//					mOnitemClickListener.onItemClick(v,pos);
+//				}
+//			});
+//			holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+//				@Override
+//				public boolean onLongClick(View v) {
+////					int pos = holder.getLayoutPosition();
+////					mOnitemClickListener.onItemLongClick(v, pos);
+//					return false;
+//				}
+//			});
+//		}
+
 	}
 	
 	@Override
@@ -62,5 +92,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 			mTextView = (TextView) itemView.findViewById(R.id.list_item);
 		}
 	}
-	
+	public void add(int position) {
+		data.add(position, "nihaoma");
+		notifyItemInserted(position);
+	}
+	public void remove(int position) {
+		data.remove(position);
+		notifyItemRemoved(position);
+	}
+
 }
